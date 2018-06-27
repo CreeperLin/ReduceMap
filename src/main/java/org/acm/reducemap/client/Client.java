78a9fc71ba83d34cd05dc92394b2d7e695c24056
woshiyyya <1085966850@qq.com>
2018-06-27 11:29:01 +0800

    Add Client

--- src/main/java/org/acm/reducemap/client/Client.java
+++ src/main/java/org/acm/reducemap/client/Client.java
@@ -1,6 +1,13 @@
 package org.acm.reducemap.client;
 
+import org.acm.reducemap.master.ExecuteReply;
+import org.acm.reducemap.master.ExecuteRequest;
+import org.acm.reducemap.master.NewWorkReply;
+import org.acm.reducemap.master.NewWorkRequest;
+
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.logging.Logger;
 
 public class Client {
@@ -12,8 +19,29 @@ public class Client {
         client = new ClientRPCClient(masterAddr,masterPort);
     }
 
-    private void run() throws InterruptedException{
+    static List<String> Paths = new LinkedList<>();
+    static List<String> Params = new LinkedList<>();
 
+    private void demo(){
+        for(int i = 0; i < 10; i++){
+            Paths.add("./work" + i +".py");
+            Params.add(" " + i + ";");
+        }
+    }
+
+    private void run() throws InterruptedException{
+        NewWorkReply reply;
+        ExecuteReply reply1;
+        for(int i = 0; i < Paths.size(); i++){
+            reply = (NewWorkReply) client.call("newWork",
+                    NewWorkRequest.newBuilder().setName("work" + i).setExecHandle(Paths.get(i)).build());
+
+            int worktype = reply.getWorkType();
+            do{
+                reply1 = (ExecuteReply) client.call("execute",
+                        ExecuteRequest.newBuilder().setWorkType(worktype).setParamHandle(Params.get(i)).build());
+            }while(reply1.getStatus() != 0);
+        }
         client.shutdown();
     }
 
@@ -24,6 +52,10 @@ public class Client {
         }
         String masterAddr = args[0];
         int masterPort = Integer.parseInt(args[1]);
+        //demo
+        Paths.add(args[2]);
+        Params.add(args[3]);
+
         System.out.println("master addr:"+masterAddr+" port:"+masterPort);
         Client client = new Client(masterAddr,masterPort);
         client.run();

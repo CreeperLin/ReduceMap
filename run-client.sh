a64bbde5f69d92d60cf986f156db7d58a9af11a8
CreeperLin <965936317@qq.com>
2018-06-27 17:33:07 +0800

    run script

---
+++ run-client.sh
@@ -0,0 +1,1 @@
+java -cp ./target/ReduceMap-1.0-SNAPSHOT-jar-with-dependencies.jar org.acm.reducemap.client.Client $@
--- run-master.sh
+++ run-master.sh
@@ -1,1 +1,1 @@
-java -classpath ./target/ReduceMap-1.0-SNAPSHOT-jar-with-dependencies.jar:./target/classes org.acm.reducemap.master.Master $@
+java -cp ./target/ReduceMap-1.0-SNAPSHOT-jar-with-dependencies.jar org.acm.reducemap.master.Master $@
